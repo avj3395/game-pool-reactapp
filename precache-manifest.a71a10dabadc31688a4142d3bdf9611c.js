7b922b16c82effe6c49443ad143a49470b7af5d8
Akshay <avj3395@gmail.com>
2020-10-23 09:33:04 +0530

    Updates

---
+++ precache-manifest.a71a10dabadc31688a4142d3bdf9611c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ade68aaec9883a6569449232648480d",
+    "url": "/game-pool-reactapp/index.html"
+  },
+  {
+    "revision": "f92810cd967a42256df6",
+    "url": "/game-pool-reactapp/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "96a530b04ec32154d4ed",
+    "url": "/game-pool-reactapp/static/css/main.5f9820ed.chunk.css"
+  },
+  {
+    "revision": "f92810cd967a42256df6",
+    "url": "/game-pool-reactapp/static/js/2.5af2ba9e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/game-pool-reactapp/static/js/2.5af2ba9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96a530b04ec32154d4ed",
+    "url": "/game-pool-reactapp/static/js/main.aae714e3.chunk.js"
+  },
+  {
+    "revision": "f04d30cba532ed5f1353",
+    "url": "/game-pool-reactapp/static/js/runtime-main.0b07cc19.js"
+  }
+]);
